a49d5acf15a39ee7d806a51e096195f8c7d5bb34
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-03-24 17:07:43 +0900

    feat: last commit

--- workspaces/admin/src/index.tsx
+++ workspaces/admin/src/index.tsx
@@ -31,7 +31,7 @@ export const AdminApp: React.FC = () => {
   }, [toast]);
 
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<h1>Something went wrong</h1>}>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider>
           <RouterProvider router={router()} />
